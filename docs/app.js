f295f1b6a3262bcd814bad39b80aba4f0c056ad8
BrydenSmith <98810585+BrydenSmith@users.noreply.github.com>
2026-03-20 21:48:21 +0000

    no light mode on street map

--- docs/app.js
+++ docs/app.js
@@ -63,10 +63,10 @@ function setMapViewAroundPin(lat, lng, miles = 20) {
 function applyLeafletBaseLayer() {
   if (!map) return;
   const isLight = getBool("light", false);
-  const url = isLight
-    ? "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-    : "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
-
+  //const url = isLight
+   // ? "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+   // : "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
+  const url = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
   const opts = {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: "abcd",
